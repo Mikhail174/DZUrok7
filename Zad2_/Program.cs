a1aeed766ce5cb82238b8727b26acaffd665b7ff
Mikhail <popovms1207@gmail.com>
2018-10-23 18:55:04 +0300

    //

--- Zad2_/Program.cs
+++ Zad2_/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace Zad2_
 {
@@ -11,24 +12,85 @@ namespace Zad2_
     {
         static void Main(string[] args)
         {
-            ShopDB shopDB = new ShopDB();
-            var employeesTableAdapter = new ShopDBTableAdapters.EmployeesTableAdapter();
-            employeesTableAdapter.Fill(shopDB.Employees);
-            var ordersTableAdapter = new ShopDBTableAdapters.OrdersTableAdapter();
-            ordersTableAdapter.Fill(shopDB.Orders);
+            // string connectionString = @"Data Source=WKS456\SQLEXPRESS;Initial Catalog=ShopDB;Integrated Security=True";
+            //string commandString = "SELECT EmployeeID FROM Employees; SELECT * FROM Orders;";
+            DataSet simpleShopDb = new DataSet();
+            simpleShopDb.ReadXmlSchema(@"E:\ADO.NET\DATA\ShopDbSchema.xml");
+            simpleShopDb.ReadXml(@"E:\ADO.NET\DATA\ShopDBData.xml");
 
-            foreach (var employee in shopDB.Employees)
+
+           // SqlDataAdapter adapter = new SqlDataAdapter(commandString, connectionString);
+            //adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            //adapter.Fill(simpleShopDb);
+            DataTable Employees = simpleShopDb.Tables[0];
+            DataTable orders = simpleShopDb.Tables[1];
+            simpleShopDb.Relations.Add("Employees_Orders", Employees.Columns["EmployeeID"], orders.Columns["EmployeeID"]);
+            Employees.Columns.Add("CountSale", typeof(double), "Count(Child(Employees_Orders).EmployeeID)");
+
+            foreach (DataRow EmployeesRow in Employees.Rows)
             {
-                Console.WriteLine("{0} {1} {2}", employee.LName, employee.FName, employee.MName);
+                foreach (DataColumn EmployeesColumn in Employees.Columns)
+                    Console.WriteLine("{0}: {1}", EmployeesColumn.ColumnName, EmployeesRow[EmployeesColumn]);
+
                 Console.WriteLine();
-
-                var orders = employee.GetOrdersRows();
-                foreach (var order in orders)
-                {
-                    Console.WriteLine("\tOrderId:{0}, {1}", order.OrderID, order.OrderDate);
-
-                }
             }
+
+
+            ShopDB strongedShopDB = new ShopDB();
+            strongedShopDB.Customers.Merge(simpleShopDb.Tables["Employees"]);
+            
+
+            foreach (DataRow row in strongedShopDB.Employees.Rows)
+            {
+                foreach (DataColumn column in strongedShopDB.Employees.Columns)
+                    Console.WriteLine("{0}: {1}", column.ColumnName, row[column]);
+
+                Console.WriteLine();
+            }
+
+            //var employeesTableAdapter = new ShopDBTableAdapters.EmployeesTableAdapter();
+            //employeesTableAdapter.Fill(strongedShopDB.Employees);
+
+            //var ordersTableAdapter = new ShopDBTableAdapters.OrdersTableAdapter();
+            //ordersTableAdapter.Fill(strongedShopDB.Orders);
+
+            //var orderDetailsTableAdapter = new ShopDBTableAdapters.OrderDetailsTableAdapter();
+            //orderDetailsTableAdapter.Fill(strongedShopDB.OrderDetails);
+
+            //ShopDB.EmployeesDataTable employees = employeesTableAdapter.GetData();
+            //ShopDB.OrdersDataTable orders1 = ordersTableAdapter.GetData();
+            //ShopDB.OrderDetailsDataTable orderDetails = orderDetailsTableAdapter.GetData();
+
+
+          //  orders.Columns.Add("TotalSold", typeof(double), "SUM(Child(Orders_OrderDetails).TotalPrice)"); // добавление расчитываемого столбца, используемого связь 
+
+            //shopDB.Relations.Add("Employees_Orders", employees.Columns["EmployeeID"], orders.Columns["EmployeeID"]);
+            //employees.Columns.Add("AggregateColumn", typeof(double), "Count(Child(FK_Orders_Employees).EmployeeID)");
+
+            // shopDB.Relations.Add("Employees_Orders", Employees.Columns["EmployeeID"], orders.Columns["EmployeeID"]);
+            // Employees.Columns.Add("CountSale", typeof(double), "Count(Child(Employees_Orders).EmployeeID)");
+            //employees.Columns.Add("AggregateColumn", typeof(double), "Count(Child(FK_Orders_Employees).EmployeeID)");
+            //employees.Columns.Add("CountSale", typeof(double), "Count(Child(Employees_Orders).EmployeeID)");
+            //foreach (DataRow employeeRow in employees.Rows)
+            //{
+            //    foreach (DataColumn employeeColumn in employees.Columns)
+            //        Console.WriteLine("{0}: {1}", employeeColumn.ColumnName, employeeRow[employeeColumn]);
+
+            //    Console.WriteLine();
+            //}
+
+            //foreach (var employee in shopDB.Employees)
+            //{
+            //    Console.WriteLine("{0} {1} {2}", employee.LName, employee.FName, employee.LName);
+            //    Console.WriteLine();
+
+            //    var orders1 = employee.GetOrdersRows();
+            //    foreach (var order in orders1)
+            //    {
+            //        Console.WriteLine("\tOrderId:{0}, {1}", order.OrderID, order.OrderDate); //
+
+            //    }
+            //}
         }
     }
 }
